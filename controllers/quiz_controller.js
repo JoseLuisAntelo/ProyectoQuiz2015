74f85d629bec12ec74d990579408f2a0af7193ea
Jose Luis Antelo <jlantelo@gmail.com>
2015-07-09 13:13:28 +0200

    Se añade Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,3 +47,21 @@ exports.index = function(req, res) {
 	};
 };
 
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build( //Crea un objeto quiz
+		{pregunta:"Pregunta", respuesta:"Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+	// guarda en la DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+			res.redirect('/quizes');
+		}) // Redirección HHTP (URL relativo). Lista de preguntas
+
+};
+
