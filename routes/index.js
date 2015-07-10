c09f7e380a3e61be3e78fcf46d121dfb74f4b058
Jose Luis Antelo <jlantelo@gmail.com>
2015-07-10 13:18:14 +0200

    Agregado editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
-
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 
 router.get('/author', function(req, res) {
 	res.render('author', {autores: [{nombre: 'Jose Luis Antelo', urlfoto: '/images/jose.jpg', urlvideo:'/videos/PlayasMallorca.mp4'}, {nombre: 'Enjuto Mojamuto', urlfoto: '/images/enjuto.jpg'}], errors: []});
